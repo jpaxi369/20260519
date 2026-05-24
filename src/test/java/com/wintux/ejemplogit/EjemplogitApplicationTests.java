8d678c6b782bdb2de3a0b2d51a2622c9ff554346
jpra86 <paxirojasjorge@gmail.com>
2026-05-23 22:24:35 -0400

    Primer Commit

---
+++ src/test/java/com/wintux/ejemplogit/EjemplogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wintux.ejemplogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
