6b558b03edc0be4f1cabc1f996a5ba64c40aba2e
jpra86 <paxirojasjorge@gmail.com>
2026-05-28 22:24:08 -0400

    Proyecto funcionando localmente

--- src/main/java/com/wintux/ejemplogit/HolaController.java
+++ src/main/java/com/wintux/ejemplogit/HolaController.java
@@ -1,11 +1,13 @@
-package com.ejemplo.demo;
+package com.wintux.ejemplogit;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+@RestController
+public class HolaController {
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "Hola mundo";
     }
 }
