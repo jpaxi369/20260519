4f915fb5c060774c749973676638061822e52fb3
jpra86 <paxirojasjorge@gmail.com>
2026-05-31 00:51:57 -0400

    agregando secretos mediante variables de entorno

--- src/main/java/com/wintux/ejemplogit/HolaController.java
+++ src/main/java/com/wintux/ejemplogit/HolaController.java
@@ -1,13 +1,24 @@
 package com.wintux.ejemplogit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HolaController {
-
+    @Value("${DB_PASSWORD}")
+    private String password;
     @GetMapping("/hola")
-    public String hola() {
-        return "Hola mundo";
+    public String saludar() {
+        return "Hola desde Spring Boot!!";
+    }
+    @GetMapping("/adios")
+    public String despedirse() {
+        return "Hasta luego! desde Spring Boot!! usando nueva rama (ej. Pull request)";
+    }
+    @GetMapping("/hola2")
+    public String saludaPersonalizado() {
+        System.out.println("El valor de la ENV es: "+password);
+        return "Hola "+password+", desde Spring Boot!!";
     }
 }
